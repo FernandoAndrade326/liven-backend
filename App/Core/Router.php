ff3e1421eff4b4318923cad09b59f44c1f00e0b9
Fernando326 <fernando326andrade@gmail.com>
2024-06-27 20:51:49 -0300

    chore: changed error text and insert nomenclature

--- App/Core/Router.php
+++ App/Core/Router.php
@@ -16,12 +16,13 @@ class Router{
         
         $url = $this->parseURL();
 
-        if(file_exists("../App/Controllers/" . ucfirst($url[1]) . ".php")){ //verifica se o controller existe - converte o c da url para upperCase
+        if(file_exists("../App/Controllers/" . ucfirst($url[3]) . ".php")){ //verifica se o controller existe - converte o c da url para upperCase
 
-            $this->controller = $url[1];
-            unset($url[1]);
+            $this->controller = $url[3];
+            unset($url[3]);
 
-        }elseif(empty($url[1])){ //se url vazia, mostra uma mensagem para situar o usuário
+
+        }elseif(empty($url[3])){ //se url vazia, mostra uma mensagem para situar o usuário
 
             echo "Olá, API do Teste Backend Liven - Por favor, digite um método na URL";
 
@@ -30,7 +31,11 @@ class Router{
         }else{
             http_response_code(404); // caso 404 retorna mensagem de erro avisando que o rec nao existe
             echo json_encode(["erro" => "Recurso não encontrado"]);
+
+            exit;
         }
+
+        //caso exista e validação ocorra, instanciamos o objeto do controlador
 
         require_once "../App/Controllers/" . ucfirst($this->controller) . ".php";
 
@@ -38,28 +43,30 @@ class Router{
 
         $this->method = $_SERVER["REQUEST_METHOD"];
 
+        //agora com base no metodo selecionado, fazemos as tratativas de acionamento
+
         switch($this->method){
             case "GET":
 
-                if(isset($url[2])){
+                if(isset($url[4])){ //caso tiver parametro enviamos para o metodo find (que fará tratativas de acordo)
                     $this->controllerMethod = "find";
-                    $this->params = [$url[2]];
-                }else{
+                    $this->params = [$url[4]];
+                }else{ //senao fazemos a listagem completa, que tem nomenclatura index
                     $this->controllerMethod = "index";
                 }
                 
                 break;
 
-            case "POST":
-                $this->controllerMethod = "store";
+            case "POST": //caso for post, redireciona para o metodo store, responsavel por adicionar ao banco
+                $this->controllerMethod = "insert";
                 break;
 
             case "PUT":
                 $this->controllerMethod = "update";
-                if(isset($url[2]) && is_numeric($url[2])){
-                    $this->params = [$url[2]];
+                if(isset($url[4]) && is_numeric($url[4])){ //verifica se tem parametro na URL
+                    $this->params = [$url[4]]; //se tiver, params, recebe o conteudo na posicao de parametro da url
                 }else{
-                    http_response_code(400);
+                    http_response_code(400); //senao, tratativa informando que é necessario ID
                     echo json_encode(["erro" => "Por favor, informe um id para ser atualizado"]);
                     exit;
                 }
@@ -67,27 +74,30 @@ class Router{
 
             case "DELETE":
                 $this->controllerMethod = "delete";
-                if(isset($url[2]) && is_numeric($url[2])){
-                    $this->params = [$url[2]];
+                if(isset($url[4]) && is_numeric($url[4])){ //verifica se há parametros na url 
+                    $this->params = [$url[4]];
                 }else{
-                    http_response_code(400);
+                    http_response_code(400); //caso nao houver, status 400 e informa que é necessario um id
                     echo json_encode(["erro" => "Por favor, informe um id para ser excluído"]);
                     exit;
                 }
                 break;
 
-            default: 
+            default: // se for diferente dos 4 metodos, informa que nao existe essa funcionalidade
                 echo "Método não suportado";
                 exit;
                 break;
         }
 
         call_user_func_array([$this->controller, $this->controllerMethod], $this->params);
-        
+        //passamos aqui qual é o controller, qual metodo setado no case sw e parametros caso houver
     }
+
+    //alterar parse para pegar query params
 
     private function parseURL(){
         return explode("/", $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
+        //explode na url pela "barra", para separar os itens
     }
 
 }
