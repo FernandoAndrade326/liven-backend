ae2c94b90a7b5aeb34ecb28dc433ea4b03235350
Fernando326 <fernando326andrade@gmail.com>
2024-06-27 20:54:58 -0300

    feat: add insert function

--- App/Models/Address.php
+++ App/Models/Address.php
@@ -30,5 +30,38 @@
             }
             
         }
+
+        public function insert(){
+            $sql = "INSERT INTO address 
+            (user_id,
+                street,
+                number,
+                complement,
+                neighborhood,
+                city,
+                state,
+                zip_code,
+                country)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+                $stmt = Model::getConn()->prepare($sql);
+                $stmt->bindValue(1, $this->user_id);
+                $stmt->bindValue(2, $this->street);
+                $stmt->bindValue(3, $this->number);
+                $stmt->bindValue(4, $this->complement);
+                $stmt->bindValue(5, $this->neighborhood);
+                $stmt->bindValue(6, $this->city);
+                $stmt->bindValue(7, $this->state);
+                $stmt->bindValue(8, $this->zip_code);
+                $stmt->bindValue(9, $this->country);
+
+                if ($stmt->execute()){
+                    //$this->id =Model::getConn()->getLastAddress();
+                    return $this;
+                } else{
+                    print_r($stmt->errorInfo());
+                    return null;
+                }
+        }
     }
 ?>
