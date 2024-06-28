187b5d287b3deec9a49537e29c9d647f218c8a26
Fernando326 <fernando326andrade@gmail.com>
2024-06-27 21:00:29 -0300

    refactor: changes on Secret_Key variable

--- App/Models/AuthService.php
+++ App/Models/AuthService.php
@@ -24,7 +24,7 @@
                     ];
 
                     
-                    define('SECRET_KEY', 'Teste_Liven_Secret_Sample');
+                    define('SECRET_KEY', 'Teste_Liven_Secret_Sample_OK');
                     //Não é uma boa prática e deve ser aleatória,
                     //mas por limitações de ambiente, e proposito didatico, coloquei aqui
                     $algorithm = 'HS256';
