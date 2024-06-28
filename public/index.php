7fef4c51058e78cc442bba6e685c0ff68dc6796a
Fernando326 <fernando326andrade@gmail.com>
2024-06-27 21:05:49 -0300

    feat: set default time_zone to brasil/sp

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo "Eai";
+    require_once("../vendor/autoload.php"); //autoload para carregar classes
+
+    date_default_timezone_set("America/Sao_Paulo");
+
+    header("Content-type: application/json"); //seta o tipo de conteudo da pagina para interpretar JSON
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header("Access-Control-Allow-Headers: Content-Type");
+
+    new App\Core\Router(); //chama o construtor do router, reponsavel pelas rotas
 
 ?>
