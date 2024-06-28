7c839a75fa20418add98be19a9d12a3542b7db36
Fernando326 <fernando326andrade@gmail.com>
2024-06-27 22:19:20 -0300

    refactor: changes on error handling message

---
+++ App/Controllers/AuthServices.php
@@ -0,0 +1,27 @@
+<?php
+
+    use App\Core\Controller;
+    
+    class AuthServices extends Controller{
+
+        public function insert(){
+            $newLogin = $this->getRequestBody();
+
+            $AuthServicesModel = $this->model("AuthService");
+            $AuthServicesModel->username = $newLogin->username;
+            $AuthServicesModel->password = $newLogin->password;
+
+            $AuthServicesModel = $AuthServicesModel->login($AuthServicesModel->username, $AuthServicesModel->password);
+
+            if($AuthServicesModel){
+                http_response_code(201);
+                echo json_encode($AuthServicesModel);
+            } else{
+                http_response_code(500);
+                echo json_encode(["Erro: " =>"Dados inconsistentes. Não foi possível realizar o login!"]);
+            }
+
+            
+        }
+    }
+?>
