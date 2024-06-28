3518c2a1e311264c1224a09fa4dbb92b4cb77e34
Fernando326 <fernando326andrade@gmail.com>
2024-06-28 10:33:23 -0300

    fix: changed tokenData's userid return

--- App/Models/AuthService.php
+++ App/Models/AuthService.php
@@ -4,40 +4,36 @@
     use App\Core\Model;
 
     class AuthService{
-        public function login($username, $password){
 
+        public function login($username, $password){
             $sql = "SELECT * FROM user WHERE username = ?";
+
             $stmt = Model::getConn()->prepare($sql);
             $stmt->bindValue(1, $username);
             $stmt->execute();
 
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            if($stmt->rowCount()>0){
+                $result = $stmt->fetch(PDO::FETCH_OBJ);
+                
+                if(password_verify($password, $result->password)){
+                $tokenData = [
+                    'user_id' => $result->id,
+                    'username' => $result->username,
+                    // Você pode adicionar mais dados relevantes ao token, se necessário
+                ];
 
-            if($user){
-                // Verifica se a senha fornecida corresponde à senha criptografada armazenada
-                if(password_verify($password, $user['password'])){
-                    // Dados do token
-                    $tokenData = [
-                        'user_id' => $user['id'],
-                        'username' => $user['username'],
-                        // Você pode adicionar mais dados relevantes ao token, se necessário
-                    ];
+                $algorithm = 'HS256';
 
-                    $algorithm = 'HS256';
-
-                    // Gera o token JWT
-                    $jwt = JWT::encode($tokenData, SECRET_KEY, $algorithm); 
+                $jwt = JWT::encode($tokenData, SECRET_KEY, $algorithm); 
+                    var_dump($tokenData);
                     // Retorna o token JWT
                     return $jwt;
                 } else {
                     return false; // Senha incorreta
                 }
-            } else {
-                return null; // Usuário não encontrado
+            } else{
+                return [];
             }
         }
-
-        
-
     }
 ?>
