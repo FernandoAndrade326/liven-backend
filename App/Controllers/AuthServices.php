9bcab7a00283c5269fe7d9765a5ae6b1e8e2bb57
Fernando326 <fernando326andrade@gmail.com>
2024-06-30 10:19:26 -0300

    chore: corrected code identation

--- App/Controllers/AuthServices.php
+++ App/Controllers/AuthServices.php
@@ -6,7 +6,7 @@
 
         public function insert(){
             $newLogin = $this->getRequestBody();
-
+            
             $AuthServicesModel = $this->model("AuthService");
             $AuthServicesModel->username = $newLogin->username;
             $AuthServicesModel->password = $newLogin->password;
@@ -15,13 +15,12 @@
 
             if($AuthServicesModel){
                 http_response_code(201);
+                echo json_encode(["Sucesso:" => "Login bem-sucedido!"]);
                 echo json_encode($AuthServicesModel);
             } else{
                 http_response_code(500);
                 echo json_encode(["Erro: " =>"Dados inconsistentes. Não foi possível realizar o login!"]);
             }
-
-            
         }
     }
 ?>
