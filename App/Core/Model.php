507e1cc2b47f9208263359e49ec06442965126aa
Fernando326 <fernando326andrade@gmail.com>
2024-07-01 15:37:47 -0300

    chore: added commenting on methods

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -2,17 +2,19 @@
 
 namespace App\Core;
 
-class Model {
+class Model
+{
 
-    private static $conexao;
+    private static $conexao; //cria nossa variavel de conexao
 
-    public static function getConn(){
+    public static function getConn() //funcao para conectar no banco
+    {
 
-        if(!isset(self::$conexao)){ //verifica se há uma conexao existente
+        if (!isset(self::$conexao)) { //verifica se há uma conexao existente
+            //caso nao houver conexao, instancia uma nova na variavvel conexao, senao mantem a existente
             self::$conexao = new \PDO("mysql:host=localhost;port=3306;dbname=liven;", "root", "");
         }
-
+        //apos rodar o PDO pra conectar com o db, retorna conexao
         return self::$conexao;
     }
-
 }
