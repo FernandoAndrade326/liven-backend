2463964d393dfcdb2ec3e7581b412ae489ec3e30
Fernando326 <fernando326andrade@gmail.com>
2024-07-01 15:31:33 -0300

    chore: added code commenting

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 
     define('SECRET_KEY', 'Teste_Liven_Secret_Sample_OK');
-
+    //seta o valor de secret key com uma key ficticia para uso do jwt
+    //essa chave é apenas didatica e nao deve ser usada com valores frageis dessa forma
 ?>
