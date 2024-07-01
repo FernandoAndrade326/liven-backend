ea65cb44a485e1909404892df2dc52c7b118019e
Fernando326 <fernando326andrade@gmail.com>
2024-07-01 15:40:27 -0300

    cleanup: removed excess of code

--- App/Core/Controller.php
+++ App/Core/Controller.php
@@ -2,18 +2,18 @@
 
 namespace App\Core;
 
-class Controller{
+class Controller
+{
 
-    public function model($model){
+    public function model($model){ //metodo para facilitar a instanciação dos models, evitar redundancia de codigo
         require_once "../App/Models/" . $model . ".php";
         return new $model;
     }
 
-    protected function getRequestBody(){
+    protected function getRequestBody(){ //metodo para retornar o body da requisição em JSON, ja tratado
         $json = file_get_contents("php://input");
         $obj = json_decode($json);
-    
-        return $obj;
-      }
 
+        return $obj;
+    }
 }
