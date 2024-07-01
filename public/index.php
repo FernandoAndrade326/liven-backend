657d64ced65b729af38c0eb62b2a0c269fedfb58
Fernando326 <fernando326andrade@gmail.com>
2024-07-01 15:33:11 -0300

    refactor: removed OPTIONS from header allowed methods

--- public/index.php
+++ public/index.php
@@ -1,15 +1,13 @@
 <?php
 
-    require_once("../config.php");
-    require_once("../vendor/autoload.php"); //autoload para carregar classes
+require_once("../config.php"); //chama nossa key ficticia
+require_once("../vendor/autoload.php"); //autoload para carregar classes
 
-    date_default_timezone_set("America/Sao_Paulo");
+date_default_timezone_set("America/Sao_Paulo");
 
-    header("Content-type: application/json"); //seta o tipo de conteudo da pagina para interpretar JSON
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header("Access-Control-Allow-Headers: Content-Type");
+header("Content-type: application/json"); //seta o tipo de conteudo da pagina para interpretar JSON
+header('Access-Control-Allow-Origin: *'); //tratativa de CORS, caso necessario 
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');//seta os tipos de endpoints suportados
+header("Access-Control-Allow-Headers: Content-Type");
 
-    new App\Core\Router(); //chama o construtor do router, reponsavel pelas rotas
-
-?>
+new App\Core\Router(); //chama o construtor do router, reponsavel pelas rotas e leitura da url
