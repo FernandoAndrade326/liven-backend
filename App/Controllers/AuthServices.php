513e05f689f7b82700d2daa88b11acbe1abd3694
Fernando326 <fernando326andrade@gmail.com>
2024-07-02 14:34:25 -0300

    cleanup: code review and commenting

--- App/Controllers/AuthServices.php
+++ App/Controllers/AuthServices.php
@@ -5,20 +5,20 @@
     class AuthServices extends Controller{
 
         public function insert(){
-            $newLogin = $this->getRequestBody();
+            $newLogin = $this->getRequestBody(); //pega o body da requisicao
             
-            $AuthServicesModel = $this->model("AuthService");
-            $AuthServicesModel->username = $newLogin->username;
+            $AuthServicesModel = $this->model("AuthService"); //instancia o objeto de authservice
+            $AuthServicesModel->username = $newLogin->username; //seta as variaveis com valores do body
             $AuthServicesModel->password = $newLogin->password;
 
             $AuthServicesModel = $AuthServicesModel->login($AuthServicesModel->username, $AuthServicesModel->password);
-
-            if($AuthServicesModel){
+            //chama o metodo de login com as variaveis do body passadas
+            if($AuthServicesModel){ //se true, roda 201 e mensagem com token JWT
                 http_response_code(201);
-                echo json_encode(["Sucesso:" => "Login bem-sucedido!"]);
+                echo json_encode(["message:" => "Login bem-sucedido!"]);
                 echo json_encode($AuthServicesModel);
             } else{
-                http_response_code(500);
+                http_response_code(500);//caso de bad request, aviso de nao possivel
                 echo json_encode(["Erro: " =>"Dados inconsistentes. Não foi possível realizar o login!"]);
             }
         }
